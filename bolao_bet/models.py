6efdc4c4c97c5c1f57ab5e6834576913297fc1e0
jmarcelo <jpereira320@gmail.com>
2016-08-11 00:11:44 -0300

    updatind bolao_bet

--- bolao_bet/models.py
+++ bolao_bet/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from F1Info.models import GPInfo, PilotInfo
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
-# Create your models here.
+DEFAULT_PILOT_ID = 1
+
+
+class UserBet(models.Model):
+    # Who
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=DEFAULT_PILOT_ID)
+    # When
+    date = models.DateTimeField(default=timezone.now)
+    # Where
+    GPrix = models.ForeignKey(GPInfo, on_delete=models.CASCADE)
+    
+    # Bet
+    pole = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='pole', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p1 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p1', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p2 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p2', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p3 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p3', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p4 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p4', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p5 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p5', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p6 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p6', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p7 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p7', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p8 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p8', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p9 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p9', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+    p10 = models.ForeignKey(PilotInfo, on_delete=models.PROTECT, related_name='p10', default=DEFAULT_PILOT_ID,
+        limit_choices_to={'active': True})
+
+    def get_absolute_url(self):
+        return reverse('F1Bet:make-post', args=(self.id,))
+    
+    def __str__(self):
+        return self.GPrix.country + self.GPrix.year.__str__()
--- bolao_bet/views.py
+++ bolao_bet/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from bolao_info.models import GPInfo, PilotInfo
+from bolao_bet.models import UserBet
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from bolao_main.models import Blog
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.utils.text import slugify
 
-# Create your views here.
+
+class BetCreate(CreateView):
+    model = UserBet
+    template_name = 'bolao_bet/userbet_form.html'
+    fields = ['GPrix', 'pole', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10']
+
+
+def make_post(request, bet_id):
+    
+    bet = get_object_or_404(UserBet, pk=bet_id)
+    
+    # import pdb;
+    # pdb.set_trace()
+    
+    try:
+        bet_user = ''
+        
+        try:
+            bet_user = bet.user.get_short_name()
+        except IOError:
+            bet_user = ''
+        
+        if bet_user == '':
+            bet_user = bet.user.username
+        
+        bet_gprix = bet.GPrix.__str__()
+        bet_date = bet.date.__str__()
+        
+        bet_pole = bet.pole.name
+        bet_p1 = bet.p1.name
+        bet_p2 = bet.p2.name
+        bet_p3 = bet.p3.name
+        bet_p4 = bet.p4.name
+        bet_p5 = bet.p5.name
+        bet_p6 = bet.p6.name
+        bet_p7 = bet.p7.name
+        bet_p8 = bet.p8.name
+        bet_p9 = bet.p9.name
+        bet_p10 = bet.p10.name
+    
+    except (KeyError, UserBet.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'bolao_bet:f1bet-add', {
+            'error_message': "Invalid bet.",
+        })
+    else:
+    
+        post = Blog()
+        post.title = 'Aposta de ' + bet_user
+        post.slug = slugify(post.title + bet_date)
+        post.body = (bet_gprix + '\n\n' + 'Pole: ' + bet_pole + '\n' +
+                     'P1: ' + bet_p1 + '\n' +
+                     'P2: ' + bet_p2 + '\n' +
+                     'P3: ' + bet_p3 + '\n' +
+                     'P4: ' + bet_p4 + '\n' +
+                     'P5: ' + bet_p5 + '\n' +
+                     'P6: ' + bet_p6 + '\n' +
+                     'P7: ' + bet_p7 + '\n' +
+                     'P8: ' + bet_p8 + '\n' +
+                     'P9: ' + bet_p9 + '\n' +
+                     'P10: ' + bet_p10)
+      
+        post.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('bolao_main:index'))
