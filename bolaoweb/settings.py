1f6769ac0074f856ce8f7c7b0155939408e733b9
jmarcelo <jpereira320@gmail.com>
2016-08-23 23:42:55 -0300

    Adding migrations

--- bolaoweb/settings.py
+++ bolaoweb/settings.py
@@ -130,7 +130,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Code for  Heroku
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
